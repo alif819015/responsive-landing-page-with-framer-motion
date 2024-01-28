82bc528d6e9cf2f0f5315f1e54f6457c3db3baeb
Mahmud Hasan <mahmudhassan11752@gmail.com>
2024-01-28 23:44:15 +0600

    make hero section

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,10 +1,14 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
 
-const Container = ({children}) => {
-    return (
-        <div className="w-full h-full max-w-[1220px] px-5 mx-auto">
-          {children} 
-        </div>
-    );
+type TContainerProps = {
+  children: ReactNode;
+  className?: string;
+};
+const Container = ({ children, className }:TContainerProps) => {
+  return (
+    <div className={cn('w-full h-full max-w-[1230px] px-5 mx-auto', className)}>{children}</div>
+  );
 };
 
 export default Container;
