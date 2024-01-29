62fc6a84b761c075aa63048ea2c4aff2755e7ddb
Mahmud Hasan <mahmudhassan11752@gmail.com>
2024-01-29 22:53:05 +0600

    used fremar motion

--- src/home/HeroSection.tsx
+++ src/home/HeroSection.tsx
@@ -1,26 +1,48 @@
 import Container from "@/components/ui/Container";
 import { Button } from "@/components/ui/button";
-import macbook from '@/assets/images/macbook-exposed.png'
+import macbook from "@/assets/images/macbook-exposed.png";
 
 const HeroSection = () => {
   return (
-    <Container className="h-[calc(100vh-64px)] grid grid-cols-1 lg:grid-cols-2 place-content-center">
-      <div>
-        <h1 className="text-8xl font-bold text-nowrap">
-          <span className="text-gray">Don't worry.</span>
-          <br />
-          <span>We'll fix it.</span>
-        </h1>
-        <p className="text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg">
-          Welcome to <span className="text-primary-foreground font-semibold">iRepair</span>, your one-stop place for all kinds of{" "}
-          <span>Macbook repairs</span> and diagnostics.
-        </p>
-        <Button>Book a service</Button>
-      </div>
-      <div className="w-3/4 lg:w-full mx-auto">
-        <img className="-rotate-[35deg] h-[95%] object-contain" src={macbook} alt="" />
-      </div>
-    </Container>
+    <div className="overflow-hidden ">
+      <Container className="h-screen pt-16 grid grid-cols-1 lg:grid-cols-2 place-content-center">
+        <MotionConfig.div variants={intro} initial="hidden" animate="visible">
+          <motion.h1
+            className="text-5xl lg:text-8xl font-bold text-nowrap"
+            variants={introChildren}
+          >
+            <span className="text-gray">Don't worry,</span>
+            <br />
+            <span>We'll fix it.</span>
+          </motion.h1>
+          <motion.p
+            className="text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg"
+            variants={introChildren}
+          >
+            Welcome to{' '}
+            <span className="text-primary-foreground font-semibold">
+              iRepair
+            </span>{' '}
+            , your one-stop place for all kinds of{' '}
+            <span className="text-primary-foreground font-semibold">
+              Macbook repairs
+            </span>{' '}
+            and diagnostics.
+          </motion.p>
+          <motion.div variants={introChildren}>
+            <Button>Book a service</Button>
+          </motion.div>
+        </MotionConfig.div>
+        <motion.div
+          className="mt-10 w-3/4 lg:w-full mx-auto"
+          variants={laptop}
+          initial="initial"
+          animate="animate"
+        >
+          <img className="h-[95%] object-contain" src={macbook} alt="" />
+        </motion.div>
+      </Container>
+    </div>
   );
 };
 
